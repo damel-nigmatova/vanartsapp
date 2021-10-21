3b44cba2df2cb6f1f22ae8c91a343b09c8652875
Nigmatova Damel <damelya_13524@mail.ru>
2021-10-21 16:14:12 -0700

    new form commit

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-const express = require("express");
-const app = express(); //initialize express
+const express = require('express');
+const app = express();
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
-const mysql = require ('mysql');
+const mysql = require('mysql');
 const path = require('path');
+const { getHomePage } = require('./routes/index');
+const { addStudentPage } = require('./routes/student');
+// set the default port 
+const PORT = 3000;
 
-const PORT = 6000;
 
 const db = mysql.createConnection({
-    host:'localhost',
-    username:'root',
-    password:'',
-    databse='vanartsapp'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'vanartsapp'
 });
 
-db.connect((err)=>{
-    if(err){
+// connect to mysql database
+
+db.connect((err) => {
+    if (err) {
         throw err;
-    }else{
-        console.log("Database connected");
     }
-})
+    console.log(" mysql database connected");
+});
 
-//make db connection global
-glodal.db = db;
+
+// make db connection global 
+global.db = db;
+
+
 //configure the middleware
-app.set('port', process.env.port || PORT); //set the port
-app.set('views', __dirname + '/views'); //set the view folder
+app.set('port', process.env.PORT || PORT); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json()); // parse form data client
+// app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+// app.use(fileUpload()); // configure fileupload
 
-app.listen(PORT, ()=>{
-    console.log('The server is running on localhost:${PORT}');
+app.get('/', getHomePage);
+app.get('/add', addStudentPage);
+
+app.listen(PORT, () => {
+    console.log(` The server is running on localhost:${PORT}`);
 })
